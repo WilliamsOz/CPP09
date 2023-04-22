3b52e02b68ceb6cfb28877134f6640ed70ee121a
wiozsert <wiozsert@student.42.fr>
2023-04-22 14:12:12 +0200

    Save

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: wiozsert <wiozsert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 13:52:02 by wiozsert          #+#    #+#             */
-/*   Updated: 2023/04/21 18:11:47 by wiozsert         ###   ########.fr       */
+/*   Updated: 2023/04/22 14:11:34 by wiozsert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,39 +48,164 @@ doit permettre de bien voir différence entre les deux contenants utilisés.
 #include "PmergeMe.hpp"
 #include <vector>
 
-// for (std::vector<int>::iterator	it = v.begin() ; it != v.end() ; it++)
-		// P(*it)
+void	printPair(std::pair<int, int>	tab[], int count) {
+	int i = 0;
 
-// void	sort(char **av, int i) {
-// 	std::vector<int>	v;
+	while (i < count) {
+		P(tab[i].first)
+		P(tab[i].second)
+		i++;
+	}
+	return ;
+}
 
-// 	while (av[i]) {
-// 		v.push_back(atoi(av[i]));
-// 		i++;
-// 	}
-// 	i = 0;
-// 	std::pair<int, int>	p[v.size()];
-// 	std::vector<int>::iterator	it = v.begin();
-// 	while (it != v.end()) {
-// 		p[i].first = *it;
-// 		it++;
-// 		p[i].second = *it;
-// 		if (it != v.end())
-// 			it++;
-// 		if (v.size() % 2 != 0 && it + 1 == v.end())
-// 			break ;
-// 		i++;
-// 	}
-// 	int count = 4;
-// 	int xd = 0;
-// 	while (count > 0) {
-// 		P(p[xd].first)
-// 		P(p[xd].second)
-// 		count--;
-// 		xd++;
-// 	}
-// 	return ;
-// }
+void	printVector(std::vector<int> v) {
+	std::vector<int>::iterator	itr;
+
+	itr = v.begin();
+	while (itr != v.end()) {
+		P(*itr)
+		itr++;
+	}
+	return ;
+}
+
+void	createPair(std::pair<int, int> pairTab[], char **av, int *tmp) {
+	int	count;
+	int	i;
+
+	count = 0;
+	i = 1;
+	while (av[i]) {
+		if (av[i + 1] == NULL) {
+			*tmp = atoi(av[i]);
+			break ;
+		}
+		pairTab[count].first = atoi(av[i]);
+		i++;
+		pairTab[count].second = atoi(av[i]);
+		i++;
+		count++;
+	}
+	return ;
+}
+
+void	sortPair(std::pair<int, int> pairTab[], int nbOfPair, int i) {
+	int	tmp;
+
+	while (i < nbOfPair) {
+		if (pairTab[i].first > pairTab[i].second) {
+			tmp = pairTab[i].first;
+			pairTab[i].first = pairTab[i].second;
+			pairTab[i].second = tmp;
+		}
+		i++;
+	}
+	return ;
+}
+
+bool	allNbPushed(std::pair<int, int> pairTab[], int nbOfPair, int i) {
+	while (i < nbOfPair) {
+		if (pairTab[i].second >= 0)
+			return false;
+		i++;
+	}
+	return true;
+}
+
+void	pushToVector(std::vector<int> *v, std::pair<int, int> pairTab[], int nbOfPair, int i) {
+	int	tmp;
+
+	tmp = pairTab[i].second;
+	if (allNbPushed(pairTab, nbOfPair, 0) == true)
+		return ;
+	while (i < nbOfPair) {
+		if (pairTab[i].second > tmp)
+			tmp = pairTab[i].second;
+		i++;
+	}
+	i = 0;
+	while (i < nbOfPair) {
+		if (tmp == pairTab[i].second)
+			pairTab[i].second = -1;
+		i++;
+	}
+	v->insert(v->begin(), tmp);
+	pushToVector(v, pairTab, nbOfPair, 0);
+	return ;
+}
+
+void	pushMinToVector(std::vector<int> *v, std::pair<int, int> pairTab[], int nbOfPair, int *keepLast) {
+	int	i;
+	int	tmp;
+
+	i = 0;
+	tmp = pairTab[i].first;
+	while (i < nbOfPair) {
+		if (pairTab[i].first < tmp)
+			tmp = pairTab[i].first;
+		i++;
+	}
+	i = 0;
+	while (i < nbOfPair) {
+		if (tmp == pairTab[i].first) {
+			if (*keepLast > 0 && tmp < *keepLast)
+				pairTab[i].first = -1;
+			else {
+				tmp = *keepLast;
+				*keepLast = -42;
+			}
+			v->insert(v->begin(), tmp);
+			break ;
+		}
+		i++;
+	}
+	return ;
+}
+
+void	addLastNb(std::vector<int> *v, std::pair<int, int> pairTab[], int NbOfPair, int keepLast) {
+	std::vector<int>::iterator	itr;
+	int							i;
+
+	i = 0;
+	while (i < NbOfPair) {
+		if (pairTab[i].first > 0) {
+			itr = std::lower_bound(v->begin(), v->end(), pairTab[i].first);
+			v->insert(itr, pairTab[i].first);
+		}
+		i++;
+	}
+	if (keepLast > 0)
+		itr = std::lower_bound(v->begin(), v->end(), keepLast);
+	return ;
+}
+
+void	sort(int ac, char **av) {
+	std::vector<int>		v;
+	std::pair<int, int>		pairTab[ac / 2];
+	int	keepLast;
+
+	keepLast = -42;
+	createPair(pairTab, av, &keepLast);
+	sortPair(pairTab, ac/2, 0);
+	pushToVector(&v, pairTab, ac/2, 0);
+	pushMinToVector(&v, pairTab, ac/2, &keepLast);
+	addLastNb(&v, pairTab, ac/2, keepLast);
+	printVector(v);
+	// printPair(pairTab, ac/2);
+	return ;
+}
+
+int main(int ac, char **av) {
+	try {
+		checkErrors(ac, av);
+		sort(ac - 1, av);
+	}
+	catch(const std::invalid_argument &e) {
+		std::cerr << REDCOLOR << e.what() << ENDCOLOR << '\n';
+	}
+	return 0;
+}
 
 /*
 
@@ -103,20 +228,3 @@ déterminer la position à laquelle chaque élément doit être inséré.
 recherche binaire = /2 jusqu'a trouver la place.
 
 */
-
-void	sort(char **av, int i) {
-
-
-	return ;
-}
-
-int main(int ac, char **av) {
-	try {
-		checkErrors(ac, av);
-		sort(av, 1);
-	}
-	catch(const std::invalid_argument &e) {
-		std::cerr << REDCOLOR << e.what() << ENDCOLOR << '\n';
-	}
-	return 0;
-}

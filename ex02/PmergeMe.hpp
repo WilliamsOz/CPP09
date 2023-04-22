61212e897d1fd418d19d90381efe8d1cc32a1410
wiozsert <wiozsert@student.42.fr>
2023-04-22 20:22:43 +0200

    End of ex02

--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: wiozsert <wiozsert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 14:10:24 by wiozsert          #+#    #+#             */
-/*   Updated: 2023/04/22 20:22:01 by wiozsert         ###   ########.fr       */
+/*   Updated: 2023/04/22 20:22:37 by wiozsert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,6 @@
 #include <deque>
 #define REDCOLOR "\033[1;31m"
 #define ENDCOLOR "\033[0m"
-
-#define P(x) std::cout << x << std::endl; //del
-void	printPair(std::pair<int, int>	tab[], int count);
-void	printVector(std::vector<int> v);
-void	printDeque(std::deque<int> d);
 
 struct timer
 {
